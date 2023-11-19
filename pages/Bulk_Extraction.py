33d43ecba557ab961212a162ad97dbc293724cda
mahardisetyoso <mahardisetyoso@gmail>
2023-11-19 16:10:36 +0700

    191123
    Adding feature st.session_state
    
    - Bulkupload
    - copy_coordinates
    - Geohash Viz

--- pages/Bulk_Extraction.py
+++ pages/Bulk_Extraction.py
@@ -7,11 +7,15 @@ from polygeohasher import polygeohasher
 from shapely.geometry import Polygon
 from shapely import wkt
 from streamlit_folium import st_folium
-from folium.plugins import Draw
-from streamlit_folium import st_folium
 
 try:
-  uploaded_files = st.file_uploader("Choose a Geojson file, Other than that will cause major ERROR!!", accept_multiple_files=False)
+  CENTER_START = [-6.189991467509655, 106.84617273604809]
+
+  #TO DEFINE SESSION STATE
+  if "center" not in st.session_state:
+    st.session_state["center"] = [-6.189991467509655, 106.84617273604809]
+
+  uploaded_files = st.file_uploader("Choose a Geojson file And Please dissolve the Files into single polygon/attribute, Other than that will cause major ERROR!!", accept_multiple_files=False)
   button = st.number_input('Insert a Geohash number')
   number = int(button)
 
@@ -21,12 +25,30 @@ try:
   gpd_geom = gpd.GeoDataFrame(gdf, geometry=gdf['geometry'], crs="EPSG:4326")
   geojson=gpd_geom.to_json()
 
+  gpd_geom['Center_point'] = gpd_geom['geometry'].centroid
+  gpd_geom["lat"] = gpd_geom.Center_point.map(lambda p: p.y)
+  gpd_geom["long"] = gpd_geom.Center_point.map(lambda p: p.x)
+
+  try:
+    latitude = float(gpd_geom["lat"])
+    longitude = float(gpd_geom["long"])
+  except ValueError:
+    latitude = None
+    longitude = None
+
+  if latitude is None or longitude is None:
+    st.error("Invalid latitude or longitude value. Please enter valid latitude and longitude values separated by a comma.")
+  else:
+    st.success("Successfully converted latitude and longitude values to float.")
+
+  st.session_state["center"] = [latitude, longitude]
+
   geohash_gdf = polygeohasher.create_geohash_list(gdf, number,inner=False)
   geohash_gdf_list = polygeohasher.geohashes_to_geometry(geohash_gdf,"geohash_list")
   gpd_geohash_geom = gpd.GeoDataFrame(geohash_gdf_list, geometry=geohash_gdf_list['geometry'], crs="EPSG:4326")
   geojson_geohash = gpd_geohash_geom.to_json()
 
-  m = folium.Map(location=[-6.175300560878783, 106.82701205991948],zoom_start=16)
+  m = folium.Map(location=st.session_state["center"],zoom_start=12)
   folium.GeoJson(
       geojson, 
       name="geojson",
@@ -42,9 +64,12 @@ try:
                                             'color': 'blue'                                          
                                       })).add_to(m)
 
+  tiles = ['Cartodb Positron','openstreetmap','Cartodb dark_matter']
+  for tile in tiles:
+    folium.TileLayer(tile).add_to(m)
 
-
-  st_folium(m, feature_group_to_add=fg, width=1200, height=800)
+  folium.LayerControl().add_to(m)
+  st_folium(m, width=1200, height=800)
 
 
 
--- pages/Geohash_Visualization_by_Copying.py
+++ pages/Geohash_Visualization_by_Copying.py
@@ -6,14 +6,38 @@ import streamlit as st
 from polygeohasher import polygeohasher
 from streamlit_folium import st_folium
 
+CENTER_START = [-6.189991467509655, 106.84617273604809]
+
+if "center" not in st.session_state:
+    st.session_state["center"] = [-6.189991467509655, 106.84617273604809]
+
 string = st.text_input('Please copy paste your Geohash separated by comma here and avoid spaces between geohash character','qqguyu7,qqguyur,qqguyu4,qqguygg,qqguyu1,qqguygr,qqguyff,qqguyuq,qqguz50,qqguyfu,qqguz52,qqguyun,qqguygm,qqguygu,qqguyg7,qqguygq,qqguygx,qqguyup,qqguyfv,qqguyuk,qqguygk,qqguyfg,qqguyut,qqguygz,qqguyfy,qqguyfc,qqguygc,qqguyg4,qqguyfz,qqguyux,qqguyg9,qqguyg3,qqguyus,qqguyuw,qqguyg1,qqguygy,qqguygs,qqguyud,qqguyu3,qqguyue,qqguz4b,qqguyg6,qqguygd,qqguyum,qqguz58,qqguygh,qqguyuj,qqguygv,qqguygj,qqguygn,qqguyu5,qqguyg5,qqguyuh,qqguygp,qqguygf,qqguyu6,qqguyu9,qqguygw,qqguyge,qqguygt')
 data_list = string.split(',')
 df = pd.DataFrame(data_list)
 df.columns = ['geohash']
+
 geohash_df_list = polygeohasher.geohashes_to_geometry(df,'geohash')
 gpd_geohash_geom = gpd.GeoDataFrame(geohash_df_list, geometry = geohash_df_list['geometry'], crs = "EPSG:4326")
 geojson_geohash = gpd_geohash_geom.to_json()
-m = folium.Map(location=[-6.176366101082727, 106.85262129873658],zoom_start=14)
+
+centroid = gpd_geohash_geom.unary_union.centroid
+
+try:
+    latitude = float(centroid.y)
+    longitude = float(centroid.x)
+except ValueError:
+    latitude = None
+    longitude = None
+
+if latitude is None or longitude is None:
+    st.error("Invalid latitude or longitude value. Please enter valid latitude and longitude values separated by a comma.")
+else:
+    st.success("Successfully converted latitude and longitude values to float.")
+
+
+st.session_state["center"] = [latitude, longitude]
+
+m = folium.Map(location=CENTER_START,zoom_start=14)
 folium.GeoJson(
      geojson_geohash, 
      name="geohash",
@@ -23,4 +47,7 @@ folium.GeoJson(
         'weight': 4,
         'interactive' : True
     }).add_to(m)
-st_data = st_folium(m, width=1200, height=800)
+st_data = st_folium(m, 
+                    center=st.session_state["center"],
+                    width=1200, 
+                    height=800)
